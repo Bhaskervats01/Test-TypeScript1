bb9e6e0bab1ec88c7ec9dfa6a35669fb226a952f
Bhaskervats01 <bhaskersharma0052@gmail.com>
2025-08-30 21:40:36 +0530

    add new file

---
+++ DAY1/app.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Number:
+let a = 10;
+let b = 20;
+//String:
+let str = "Bhasker";
+// Boolean
+let isExit = true;
+isExit = false;
+//bigint
+let bignumber = 132143292182n;
+//null
+let abc = null;
+let bcd;
+let bcd = undefined;
+// using tsc --watch (convert directly TS file to JS file):
+let names = "Mohan";
+//# sourceMappingURL=app.js.map
---
+++ DAY1/app.ts
@@ -0,0 +1,21 @@
+// Number:
+let a:number = 10;
+let b:number = 20;
+
+//String:
+let str:string ="Bhasker";
+
+// Boolean
+let isExit:boolean = true;
+isExit = false;
+
+//bigint
+let bignumber:bigint = 132143292182n;
+
+//null
+let abc:null = null;
+let bcd:undefined;
+let bcd:undefined = undefined;
+
+// using tsc --watch (convert directly TS file to JS file):
+let names:string = "Mohan";
